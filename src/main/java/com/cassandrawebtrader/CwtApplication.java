c37a83eac90901464cdc77d5eb7c4b51d164e0ed
Luis Diaz Sendel <luis.diaz.s@gmail.com>
2015-09-06 00:47:27 -0400

    Playing

--- src/main/java/com/cassandrawebtrader/CwtApplication.java
+++ src/main/java/com/cassandrawebtrader/CwtApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+/**
+ * @author lds
+ *
+ */
 @SpringBootApplication
 public class CwtApplication extends SpringBootServletInitializer {
 
